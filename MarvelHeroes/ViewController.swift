f122c219ecd8ce09f36ae94010a1eea020dce7c2
Matt Todd <matt.c.todd@gmail.com>
2016-09-10 15:46:08 -0500

    initial commit

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by mtodd on 9/10/16.
+//  Copyright © 2016 Matt Todd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
