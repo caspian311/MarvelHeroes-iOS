77d49d9215a842ba08bb2f16b21db6232fc7b5ed
Matt Todd <matt.c.todd@gmail.com>
2016-09-11 16:40:11 -0500

    changed some styling

--- MarvelHeroes/ViewController.swift
+++ MarvelHeroes/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let startColor = UIColor(red: 51/256, green: 0, blue: 0, alpha: 1)
+        let midColor = UIColor(red: 170/256, green: 0, blue: 0, alpha: 1)
+        
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [startColor.CGColor, midColor.CGColor, startColor.CGColor]
+        self.view.layer.insertSublayer(gradient, atIndex: 0)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
